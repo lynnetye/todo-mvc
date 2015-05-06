da423c5f0afc67c316709ab0c0bbe347b619711a
Lynne Tye <lynnetye@gmail.com>
2015-05-06 04:12:16 -0700

    incoporate feedback / refactor js

--- application.js
+++ application.js
@@ -1,74 +1,113 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var $newTodoInput = $('.new-todo-input'),
       clearAllCounter = 0;
 
-  $newTodoInput.on('keydown', function(event){
+  $newTodoInput.on('keydown', function(event) {
     if (event.keyCode !== 13) {
       return;
     }
-    else {
-      var enteredInput = $(this).val();
+    var enteredInput = $(this).val();
 
-      addNewTodoToList(enteredInput);
-      $(this).val('');
+    addNewTodoToList(enteredInput);
+    $(this).val('');
+  });
+
+  $('body').on('click', 'i', function() {
+    var $faIcon = $(event.target),
+        $task = $faIcon.closest('div'),
+        status = $task.attr('completed');
+
+    if ($faIcon.hasClass('complete-icon')) {
+      completeTask($faIcon, status, $task);
+      updateDisplayOfFeatures();
+
+    } else if ($faIcon.hasClass('delete-icon')) {
+      deleteTask($task);
+
+    } else if ($faIcon.hasClass('clear-all-icon')) {
+      clearAllTasks(clearAllCounter);
+      clearAllCounter += 1;
     }
   });
 
-  $('body').on('click', function() {
-    if (event.target.nodeName === 'I') {
-      var $faIcon = $(event.target),
-          $task = $faIcon.closest('div'),
-          status = $task.attr('completed');
+  $('body').on('click', 'p.filter-action', function() {
+    var $filterTab = $(event.target);
 
-      if ($faIcon.hasClass('complete-icon')) {
-        completeTask($faIcon, status, $task);
-        adjustSettings();
-
-      } else if ($faIcon.hasClass('delete-icon')) {
-        deleteTask($task);
-
-      } else if ($faIcon.hasClass('clear-all-icon')){
-        clearAllTasks(clearAllCounter);
-        clearAllCounter += 1;
-      }
-
-    } else if (event.target.nodeName === 'P') {
-      var $filterTab = $(event.target);
-
-      if ($filterTab.hasClass('clear-completed')) {
-        clearCompletedTasks();
-      }
-      applyCorrectFilter($filterTab);
+    if ($filterTab.hasClass('clear-completed')) {
+      clearCompletedTasks();
+    } else if ($filterTab.hasClass('filter-all')) {
+      showAllTasks($filterTab);
+    } else if ($filterTab.hasClass('filter-active')) {
+      showActiveTasks($filterTab);
+    } else if ($filterTab.hasClass('filter-completed')) {
+      showCompletedTasks($filterTab);
     }
   });
 });
 
-function completeTask($faIcon, status, $task) {
-  if (status === 'false') {
-    $faIcon.removeClass('fa-circle-o')
-      .addClass('fa-check-circle-o');
-    $task.attr('completed', 'true')
-      .find('input').addClass('line-through');
-  } else if (status === 'true') {
-    $faIcon.removeClass('fa-check-circle-o')
-      .addClass('fa-circle-o');
-    $task.attr('completed', 'false')
-      $task.find('input').removeClass('line-through');
-  }
-  adjustSettings();
-};
-
-function deleteTask($task) {
-  $task.remove();
-  adjustSettings();
-};
-
-function clearCompletedTasks(){
+function clearCompletedTasks() {
   var $allCompletedTasks = $allTasks().filter("[completed='true']");
 
   for (var i = 0; i < $allCompletedTasks.length; i++) {
     deleteTask($($allCompletedTasks[i]));
   }
+};
+
+function showAllTasks($filterTab) {
+  for (var i = 0; i < $allTasks().length; i++) {
+    $($allTasks()[i]).removeClass('hide');
+    showSelectedFilterTab($filterTab);
+  }
+};
+
+function showActiveTasks($filterTab) {
+  showSelectedFilterTab($filterTab);
+  for (var i = 0; i < $allTasks().length; i++) {
+    var $task = $($allTasks()[i]);
+
+    if ($task.attr('completed') === 'true') {
+      $task.addClass('hide');
+    } else {
+      $task.removeClass('hide');
+    }
+  }
+};
+
+function showCompletedTasks($filterTab) {
+  showSelectedFilterTab($filterTab);
+  for (var i = 0; i < $allTasks().length; i++) {
+    var $task = $($allTasks()[i]);
+
+    if ($task.attr('completed') === 'false') {
+      $task.addClass('hide');
+    } else {
+      $task.removeClass('hide');
+    }
+  };
+};
+
+function completeTask($faIcon, status, $task) {
+  if (status === 'false') {
+    $faIcon
+      .removeClass('fa-circle-o')
+      .addClass('fa-check-circle-o');
+    $task
+      .attr('completed', 'true')
+      .find('input').addClass('line-through');
+  } else if (status === 'true') {
+    $faIcon
+      .removeClass('fa-check-circle-o')
+      .addClass('fa-circle-o');
+    $task
+      .attr('completed', 'false')
+      $task.find('input').removeClass('line-through');
+  }
+  updateDisplayOfFeatures();
+};
+
+function deleteTask($task) {
+  $task.remove();
+  updateDisplayOfFeatures();
 };
 
 function clearAllTasks(clearAllCounter) {
@@ -84,61 +123,33 @@ function clearAllTasks(clearAllCounter) {
   }
 };
 
-function applyCorrectFilter($filterTab) {
-  for (var i = 0; i < $allTasks().length; i++) {
-    var $task = $($allTasks()[i]);
-
-    if ($filterTab.hasClass('filter-all')) {
-      $task.removeClass('hide');
-      adjustFilterSettings($filterTab);
-
-    } else if ($filterTab.hasClass('filter-active')) {
-      adjustFilterSettings($filterTab);
-      if ($task.attr('completed') === 'true') {
-        $task.addClass('hide');
-      } else {
-        $task.removeClass('hide');
-      }
-
-    } else if ($filterTab.hasClass('filter-completed')) {
-      adjustFilterSettings($filterTab);
-      if ($task.attr('completed') === 'false') {
-        $task.addClass('hide');
-      } else {
-        $task.removeClass('hide');
-      }
-    };
-  }
-};
-
 function addNewTodoToList(newTodoText) {
   var $todoClone = $('.todo-template').first().clone(),
       $todoCloneInput = $todoClone.find('input'),
       $todoCloneFaIcon = $todoClone.find('i').first();
 
   $todoCloneInput.val(newTodoText);
-  $todoClone.removeClass('todo-template')
+  $todoClone
+    .removeClass('todo-template')
     .removeClass('hide')
-    .attr('id', currentLengthOfTodoList() + 1)
     .attr('completed', 'false');
   $todoCloneFaIcon.addClass('fa-circle-o');
   $('.todo-list').append($todoClone);
-  adjustSettings();
+  updateDisplayOfFeatures();
 };
 
-function adjustSettings(){
+function updateDisplayOfFeatures() {
   var $filtersTab = $('.filters-tab'),
       $itemCounter = $('.item-counter'),
-      $completeAllIcon = $('.todo-form > i'),
-      numberOfActiveTasks = currentNumberOfActiveTasks();
+      $completeAllIcon = $('.todo-form > i');
 
   if (currentLengthOfTodoList() > 0) {
     $filtersTab.removeClass('hide');
     $completeAllIcon.attr('id', '');
-    if (numberOfActiveTasks === 1 ) {
-      $itemCounter.text(numberOfActiveTasks + ' item left');
+    if (currentNumberOfActiveTasks() === 1 ) {
+      $itemCounter.text(currentNumberOfActiveTasks() + ' item left');
     } else {
-      $itemCounter.text(numberOfActiveTasks + ' items left');
+      $itemCounter.text(currentNumberOfActiveTasks() + ' items left');
     }
   } else {
     $filtersTab.addClass('hide');
@@ -152,9 +163,10 @@ function adjustSettings(){
   }
 };
 
-function adjustFilterSettings($filterTab){
-  $filterTab.addClass('selected');
-  $filterTab.siblings().removeClass('selected');
+function showSelectedFilterTab($filterTab) {
+  $filterTab
+    .addClass('selected')
+    .siblings().removeClass('selected');
 };
 
 
@@ -164,17 +176,14 @@ function $allTasks() {
   return $('.todo-list').children().not('.todo-template, .todo-form');
 };
 
-function currentLengthOfTodoList(){
-  var numberOfTasks = $allTasks().length;
-  return numberOfTasks;
+function currentLengthOfTodoList() {
+  return $allTasks().length;
 };
 
-function currentNumberOfActiveTasks(){
-  var numberOfActiveTasks = $allTasks().filter("[completed='false']").length;
-  return numberOfActiveTasks;
+function currentNumberOfActiveTasks() {
+  return $allTasks().filter("[completed='false']").length;
 };
 
-function currentNumberOfCompletedTasks(){
-  var numberOfCompletedTasks = $allTasks().filter("[completed='true']").length;
-  return numberOfCompletedTasks;
+function currentNumberOfCompletedTasks() {
+  return $allTasks().filter("[completed='true']").length;
 };
